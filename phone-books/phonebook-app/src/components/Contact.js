6e4ecc6190e719052f608dabe0b739b9254872fc
Aisha Gench <aisha.gench@gmail.com>
2021-04-08 22:55:20 -0500

    created state object

--- phone-books/phonebook-app/src/components/Contact.js
+++ phone-books/phonebook-app/src/components/Contact.js
@@ -5,13 +5,20 @@ import Form from './Form'
 
 
 export default class Contact extends Component{
-    
+    state = {
+        contacts: [
+          {name: 'Name 1', phone: '+1234567890'},
+          {name: 'Name 2', phone: '+1265482540'},
+          {name: 'Name 3', phone: '+9834567890'},
+          {name: 'John', phone: '+5553337678'},
+        ]
+      }
+
     render(){
         return(
             <div>
-                
-                <Form/>
-                <List/>
+                <List  contacts={this.state.contacts}/>
+                <Form />
             </div>
         
 
--- phone-books/phonebook-app/src/components/List.js
+++ phone-books/phonebook-app/src/components/List.js
@@ -1,24 +1,27 @@
 import React, { Component } from 'react'
 import '../styles/List.css'
+import PropTypes from 'prop-types';
 
 export default class List extends Component {
+
+    static propTypes={
+        contacts : PropTypes.array.isRequired
+    }
+
     render() {
+        console.log(this.props.contacts)
         return (
             <div className="listCompField">
                 
                 <input name="filter" id="filter" placeholder="Search by name or phone"/>
                 <ul >
 
-                    <li>
-                        <span className="name">Name 1</span>
-                        <span className="phone">Phone 1</span>
-                    </li>
-                    <li><span className="name">Name 2</span>
-                        <span className="phone">Phone 2</span>
-                    </li>
-                    <li><span className="name">Name 3</span>
-                        <span className="phone">Phone 3</span>
-                    </li>
+                {this.props.contacts.map((item)=>{
+                 return(<li>
+              <span className="name">{item.name}</span>
+              <span className="phone">{item.phone}</span>
+               </li>)
+              })}
                 </ul>
 
             </div>
