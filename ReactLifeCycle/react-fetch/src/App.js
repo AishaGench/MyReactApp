0f34687a4027961071727b05c753bdcaf02c4461
Aisha Gench <aisha.gench@gmail.com>
2021-04-19 20:48:51 -0500

    created axios

--- ReactLifeCycle/react-fetch/src/App.js
+++ ReactLifeCycle/react-fetch/src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
 import UserList from './UserList'
+import {BrowserRouter, Route} from 'react-router-dom'
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
      <h1> APP COMPONENT </h1>
+     <Route path="/" exact strict render={()=>{return <h1>Welcome to Home Page</h1>}}/>
+     <Route path="/Contact" exact strict render={()=>{return <h1>Contact Page</h1>}}/>
      <UserList/>
     </div>
+    </BrowserRouter>
   );
 }
 
--- ReactLifeCycle/react-fetch/src/UserList.js
+++ ReactLifeCycle/react-fetch/src/UserList.js
@@ -1,18 +1,41 @@
 import React, { Component } from 'react'
+import axios from "axios"
 
 export default class UserList extends Component {
-    state={user:[]}
+    state={
+        users:[],  
+        isLoading: true
+    }
 
     componentDidMount() {
-        fetch('https://jsonplaceholder.typicode.com/users')
-  .then(response => response.json())
-  .then(json => console.log(json))
+        
+            // fetch('https://jsonplaceholder.typicode.com/users')
+            // .then(response => response.json())
+            // //.then(json => console.log(json))
+            // .then((jsonData)=>this.setState({users:jsonData})
+            // )
+
+
+            axios.get('https://jsonplaceholder.typicode.com/users')
+            .then((jsonData)=>this.setState({users:jsonData.data, isLoading:false})
+            )
+       
     }
+
     
     render() {
+        const {isLoading, users} = this.state
+        console.log(this.state.users)
         return (
             <div>
                 <h2>UserList Component</h2>
+                {isLoading === true ? "Loading..." : this.state.users.map((item)=>{
+        return <div key={item.id}> {item.name}</div>
+      })}
+
+                {/* {this.state.users.map((item)=>(
+                    <div key ={item.id}>{item.name} {item.username}</div>
+                ))} */}
             </div>
         )
     }
