987d3e0e55b98257826afca9e4f1ff9d2356b783
Aisha Gench <aisha.gench@gmail.com>
2021-04-20 21:30:11 -0500

    created new app for Higher Order Components

---
+++ ReactLifeCycle/hoc/src/Main.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class Main extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Main Component</h1>
+            </div>
+        )
+    }
+}
+
+
