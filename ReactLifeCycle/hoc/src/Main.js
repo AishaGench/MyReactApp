30f7d94fbea64391d4abb5eb88d4cd5389f255ca
Aisha Gench <aisha.gench@gmail.com>
2021-04-20 22:26:17 -0500

    created Posts js

--- ReactLifeCycle/hoc/src/Main.js
+++ ReactLifeCycle/hoc/src/Main.js
@@ -1,28 +1,29 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { Component } from 'react'
+import axios from 'axios'
+import Posts from './Posts'
 
 export default class Main extends Component {
-    state= {posts:[], loading:true}
+  state = {posts:[], loading:true}
+  componentWillMount() {
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+    .then(response => response.data)
+    .then(result =>{
+      setTimeout(() => {
+        this.setState({posts:result, loading:false})
+      }, 3000);
+    })
+  }
 
-    componentWillMount() {
-        axios.get('https://jsonplaceholder.typicode.com/posts')
-        .then(response => response.data)
-        .then(result => {
-            setTimeout(() => {
-                this.setState({posts:result, loading: false})
-
-            },3000)
-        })
-    }
-    
-    render() {
-        return (
-            <div>
-                <h1>Main Component</h1>
-                {this.state.loading === true ? "Loading ..." : "Data is been received" }
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <h1>Main Component</h1>
+        {/* {this.state.loading === true ? "Loading..." : "Data is been received."} */}
+        {/* <Posts data={this.state}/> */}
+        <Posts {...this.state}/>
+      </div>
+    )
+  }
 }
 
 
---
+++ ReactLifeCycle/hoc/src/Posts.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Posts extends Component {
+    render() {
+        console.log(this.props)
+        return (
+          <div className="postContainer">
+            {this.props.loading ? <div>Loading...</div> : this.props.posts.map(item =><div key={item.id}>{item.title}</div>)}
+          </div>
+        )
+    }
+}
