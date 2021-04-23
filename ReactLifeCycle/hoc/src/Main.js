f13c20e07bfb769885a07d4fec481419ee39f85a
Aisha Gench <aisha.gench@gmail.com>
2021-04-22 19:46:01 -0500

    created users component

--- ReactLifeCycle/hoc/src/Main.js
+++ ReactLifeCycle/hoc/src/Main.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 import Posts from './Posts'
+import Users from './Users'
 
 export default class Main extends Component {
-  state = {posts:[], loading:true}
+  state = {posts:[],users:[], loading:true}
   componentWillMount() {
     axios.get('https://jsonplaceholder.typicode.com/posts')
     .then(response => response.data)
     .then(result =>{
       setTimeout(() => {
         this.setState({posts:result, loading:false})
+      }, 3000);
+    })
+
+    axios.get('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.data)
+    .then(result =>{
+      setTimeout(() => {
+        this.setState({users:result, loading:false})
       }, 3000);
     })
   }
@@ -21,6 +30,7 @@ export default class Main extends Component {
         {/* {this.state.loading === true ? "Loading..." : "Data is been received."} */}
         {/* <Posts data={this.state}/> */}
         <Posts {...this.state}/>
+        <Users {...this.state}/>
       </div>
     )
   }
---
+++ ReactLifeCycle/hoc/src/Users.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import LoaderHoc from './LoaderHoc'
+
+class Users extends Component {
+  render() {
+    console.log(this.props)
+    return (
+      <div className="postContainer">
+        {this.props.loading ? <div>Loading...</div> : this.props.users.map(item =><div key={item.id}>{item.name}</div>)}
+      </div>
+    )
+  }
+}
+
+export default LoaderHoc(Users)
