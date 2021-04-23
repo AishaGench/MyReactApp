4390883cf712fb29c7a76dbb35f6c60484298eed
Aisha Gench <aisha.gench@gmail.com>
2021-04-22 22:04:51 -0500

    created button for two components

--- ReactLifeCycle/counter/src/ClassComponent.js
+++ ReactLifeCycle/counter/src/ClassComponent.js
@@ -9,10 +9,25 @@ export default class ClassComponent extends Component {
     }
   }
   // state ={counter:0}
+  increase = () => {
+    this.setState({counter: this.state.counter+1})
+  }
+  reset = () => {
+    this.setState({counter: 0})
+  }
+  decrease = () => {
+    this.setState({counter: this.state.counter-1})
+  }
+
   render() {
+    console.log('Class Component => ', this.state.counter )
     return (
       <div>
         <h1>Class Component</h1>
+        <h2>{this.state.counter}</h2>
+        <button onClick={this.increase}>+</button>
+        <button onClick={this.reset}>0</button>
+        <button onClick={this.decrease}>-</button>
       </div>
     )
   }
--- ReactLifeCycle/counter/src/FunctionalComponent.js
+++ ReactLifeCycle/counter/src/FunctionalComponent.js
@@ -1,8 +1,24 @@
+import React, {useState} from 'react'
+
 export default function FunctionalComponent() {
-    const [counter, setCounter] = useState(0)
-    return (
-      <div>
-        <h1>Functional Component</h1>
-      </div>
-    )
+  const [counter, setCounter] = useState(0)
+  console.log("Functional Component => ", counter)
+  const increase = () => {
+    setCounter(counter+1)
   }
+  const reset = () => {
+    setCounter(0)
+  }
+  const decrease = () => {
+    setCounter(counter-1)
+  }
+  return (
+    <div>
+      <h1>Functional Component</h1>
+      <h2>{counter}</h2>
+      <button onClick={increase}>+</button>
+      <button onClick={reset}>0</button>
+      <button onClick={decrease}>-</button>
+    </div>
+  )
+}
