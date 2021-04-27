799117fad92297521425fce09a3bb56ed0dacc55
Aisha Gench <aisha.gench@gmail.com>
2021-04-26 19:35:58 -0500

    add timer

--- ReactLifeCycle/counter/src/ClassComponent.js
+++ ReactLifeCycle/counter/src/ClassComponent.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react'
 
 export default class ClassComponent extends Component {
-  constructor(props) {
-    super(props)
+  // constructor(props) {
+  //   super(props)
   
-    this.state = {
-       counter:0 ,
-       loading: true
-    }
-  }
-  // state ={counter:0}
+  //   this.state = {
+  //      counter:0 ,
+  //      loading: true
+  //   }
+  // }
+  state ={counter:0, isVisible: false, timer:0}
   increase = () => {
     this.setState({counter: this.state.counter+1})
   }
@@ -23,11 +23,18 @@ componentDidMount() {
   console.log("Class=> componentDidMount")
 }
 componentDidUpdate(prevProps, prevState) {
+  //console.log("Class=> componentDidUpdate")
+}
+componentDidUpdate() {
   console.log("Class=> componentDidUpdate")
+  this.myTimer = setInterval(() => {
+    this.setState({timer: this.state.timer +1})
+  }, 1000);
 }
 
   render() {
     //console.log('Class Component => ', this.state.counter )
+    console.log('Timer: ',this.state.timer)
     return (
       <div>
         <h1>Class Component</h1>
