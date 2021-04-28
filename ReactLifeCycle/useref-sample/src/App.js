fe4d918215c6a0f978a79dd0645fb2053645c37a
Aisha Gench <aisha.gench@gmail.com>
2021-04-27 22:54:01 -0500

    create button

--- ReactLifeCycle/useref-sample/src/App.js
+++ ReactLifeCycle/useref-sample/src/App.js
@@ -4,11 +4,19 @@ import React, {useRef} from 'react'
 function App() {
   const inputRef = useRef()
   const divRef = useRef()
+  const onFocusBtn = () =>{inputRef.current.focus();}
+  const onDivBorderBtn = () =>{divRef.current.style.border = '2px red solid'}
+  const onViewInputValueBtn = () =>{alert (inputRef.current.value)}
+  console.log('inputRef =>', inputRef)
+  console.log('inputRef.current =>', inputRef.current)
   return (
     <div ref = {divRef} className="App">
-      
+      <h1> Welcome </h1>
+      <input ref = {inputRef} placeholder='Enter a text...'/>
+      <button onClick={onFocusBtn}>Focus on Button</button>
+      <button onClick={onDivBorderBtn}>Div Border Button</button>
+      <button onClick={onViewInputValueBtn}>View Input Value</button>
     </div>
   );
 }
-
 export default App;
