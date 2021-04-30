0795a8d17defd51eb5580a0ee4e4d067f6f66665
Aisha Gench <aisha.gench@gmail.com>
2021-04-29 21:11:06 -0500

    add reducer

--- ReactLifeCycle/useref-sample/src/App.js
+++ ReactLifeCycle/useref-sample/src/App.js
@@ -1,14 +1,26 @@
 import './App.css';
-import React, {useRef} from 'react'
+import React, {useRef, useEffect} from 'react'
+import ReducerComponent from './ReducerComponent' 
 
 function App() {
   const inputRef = useRef()
   const divRef = useRef()
+  const data = useRef(985)
+  console.log('data-> ',data)
+
   const onFocusBtn = () =>{inputRef.current.focus();}
+
   const onDivBorderBtn = () =>{divRef.current.style.border = '2px red solid'}
+
   const onViewInputValueBtn = () =>{alert (inputRef.current.value)}
-  console.log('inputRef =>', inputRef)
-  console.log('inputRef.current =>', inputRef.current)
+
+  useEffect(() => {
+    console.log("inputRef=> ", inputRef);
+    console.log("inputRef.current=> ", inputRef.current);
+    console.log("divRef=> ", divRef);
+    console.log("divRef.current=> ", divRef.current);
+  });
+
   return (
     <div ref = {divRef} className="App">
       <h1> Welcome </h1>
@@ -16,6 +28,8 @@ function App() {
       <button onClick={onFocusBtn}>Focus on Button</button>
       <button onClick={onDivBorderBtn}>Div Border Button</button>
       <button onClick={onViewInputValueBtn}>View Input Value</button>
+      <hr/>
+      <ReducerComponent />
     </div>
   );
 }
--- ReactLifeCycle/useref-sample/src/ReducerComponent.js
+++ ReactLifeCycle/useref-sample/src/ReducerComponent.js
@@ -1,24 +1,49 @@
-import React, { useState } from 'react'
+import React, { useState, useReducer } from 'react'
+
+const myInitialState = {data: "", loading: "", error: ""}
+
+const reducer =(state, action) =>{
+    switch (action.type) {
+        case "FETCH_START": return{...state, data:"", loading: true, error:""}
+        case "FETCH_SUCCESS": return {...state, data:action.payload,loading:false, error:""};
+        case "FETCH_ERROR": return {...state, loading:false, error:action.payload};
+    
+        default: return state;
+            
+    }
+}
 
 function ReducerComponent() {
-    const [data, setData] = useState("")
-    const [loading, setLoad] = useState(false)
-    const [error, setError] = useState("")
+    // const [data, setData] = useState("")
+    // const [loading, setLoad] = useState(false)
+    // const [error, setError] = useState("")
+
+    const [state, dispatch] = useReducer(reducer, myInitialState)
 
     const fetchingDog = () =>{
-        setLoad("")
-        setError("")
-        setData("")
-
+        // setLoad("")
+        // setError("")
+        // setData("")
+        dispatch({type:"FETCH_START"})
         fetch("https://dog.ceo/api/breeds/image/random")
         .then(res =>res.json())
-        .then(result => {setLoad(false); setData(result.message)})
-        .catch(() => {setError("Error in fetching data...")})
+        .then(result => {
+            // setLoad(false); 
+            // setData(result.message)
+            dispatch({type:"FETCH_SUCCESS", payload:result.message})
+        })
+        .catch(() => {
+            // setError("Error in fetching data...")
+            dispatch({type:"FETCH_ERROR", payload:"Error in fetching data..."})
+
+        })
     }
-    
+
     return (
         <div>
-            
+            <button onClick = {fetchingDog} disabled={state.loading}>Fetch Dog </button>
+            {state.data && <div><img src= {state.data} alt= "Random Dog" /></div>}
+            {state.error && <p>{state.error}</p>}
         </div>
     )
 }
