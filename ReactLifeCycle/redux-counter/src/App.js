877274ca3ba3a1088163834f50907fee79d8ba80
Aisha Gench <aisha.gench@gmail.com>
2021-05-11 22:45:51 -0500

    create buttons

--- ReactLifeCycle/redux-counter/src/App.js
+++ ReactLifeCycle/redux-counter/src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 import Counter from './Counter'
 
+
 function App() {
   return (
     <div className="App">
       <h1>App Component</h1>
-      <Counter/>
-      
+      <Counter />
     </div>
   );
 }
--- ReactLifeCycle/redux-counter/src/Counter.js
+++ ReactLifeCycle/redux-counter/src/Counter.js
@@ -1,25 +1,32 @@
+// created with rfcredux snippet
 import React from 'react'
 import { connect } from 'react-redux'
-import { decreaseCount, increaseCount } from './actions/counterAction'
+import {increaseCount, decreaseCount} from './actions/counterAction'
+import 'semantic-ui-css/semantic.min.css'
+import { Button } from 'semantic-ui-react'
 
 
 const Counter = (props) => {
-    return (
-        <div>
-           <h2>Counter Component</h2> 
-           <button onclick={()=>decreaseCount()}>-</button>
-           <span style={{padding:"10px"}}>{props.count}</span>
-           <button onClick={()=>increaseCount()}>+</button>
-        </div>
-    )
+  return (
+    <div>
+      <h2>Counter Component</h2>
+      <Button content='DECREASE' icon='minus' labelPosition='left' onClick={()=>props.decreaseCount()}/>
+      <span style={{padding:"10px"}}>{props.count}</span>
+     <Button content='INCREASE' icon='plus' labelPosition='right' onClick={()=>props.increaseCount()}/>
+  
+      {/* <button onClick={()=>props.decreaseCount()}>-</button>
+      <span style={{padding:"10px"}}>{props.count}</span>
+      <button onClick={()=>props.increaseCount()}>+</button> */}
+    </div>
+  )
 }
 
 const mapStateToProps = (state) => {
-    return {count:state.count}
+  return {count:state.count}
 }
 
 const mapDispatchToProps = {
-    increaseCount, decreaseCount
+  increaseCount, decreaseCount
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Counter)
